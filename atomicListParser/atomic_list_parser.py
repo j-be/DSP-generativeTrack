9e84c73bbcf4471e76a88d87e4ecdf1c90228869
Juri Berlanda <juriberlanda@hotmail.com>
2017-02-03 01:47:45 +0100

    [Parser] Add comments to parser script.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- atomicListParser/atomic_list_parser.py
+++ atomicListParser/atomic_list_parser.py
@@ -1,18 +1,42 @@
+""" Parser for data provided by http://www.johnstonsarchive.net/nuclear/tests/index.html.
+
+Autor: Juri Berlanda
+License: MIT
+
+This script takes the values from the right columns (they unfortunately differ in the various files), filters the data
+for explosions having more then 1 kiloton, parses the dates, maps the date to the position within the track and sorts
+the data chronologically. Finally the data is wrapped in a function call, one per row, and saved to a file, so it can
+be copied to SuperCollider.
+
+Note: I know, this script is kind of quick and dirty and a lot of copy and paste, but it does the job.
+"""
 import datetime
 
-
+# Date to be mapped to the end of the track as unix timestamp in seconds
 MAX_TIME = 2016000000
+# Length of the track in seconds
 TRACK_DURATION = 120
 
+
 def parse_date(date_str):
+    """ Date parser function for the date format used by the data source.
+
+        Maps a date string of type like '2017  JUN  12' to a date object.
+    """
     return datetime.datetime.strptime(date_str.strip(), "%Y  %b  %d")
 
 
 def relative_date(date_str):
+    """ Helper function for mapping the the date to seconds from track begin.
+
+        The date equivalent to reference_date (see below) will return 0, the date equivalent to MAX_TIME will return
+        TRACK_DURATION. Between these 2 values the dates are mapped linearly.
+    """
     return str((parse_date(date_str) - reference_date).total_seconds() / MAX_TIME * TRACK_DURATION)
 
-
+# The date equivalent to the begin of the track
 reference_date = parse_date('1945  JUL  15')
+
 parsed_list = []
 
 with open('list-us.txt') as atomic_list:
@@ -144,6 +168,7 @@ with open('list-others.txt') as atomic_list:
 # Sort the list by timestamp
 parsed_list = sorted(parsed_list, key=lambda atomic_test: float(atomic_test[0]))
 
+# Write list as function calls to a file
 with open('list-full.csv', 'w') as full_list:
     for line in parsed_list:
         full_list.write('\t~parseData.(' + ', '.join(line) + '),\n')
